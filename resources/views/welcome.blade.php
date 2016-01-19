26cf89c61d8684f9d89eb6839d4d0ef90a78fdbb
JUan Andrade <jfandradea@gmail.com>
2016-01-19 10:31:58 -0500

    test server

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">APLICACIÓN DE PRUEBA PARA INEVAL</div>
             </div>
         </div>
     </body>
